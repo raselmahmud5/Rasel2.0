f6e83f39ab14b655f6bbd4fa7c13e0219d572925
raselmahmud5 <calabobot2@gmail.com>
2026-04-26 20:45:45 +0600

    Update bet.js

--- scripts/cmds/bet.js
+++ scripts/cmds/bet.js
@@ -21,7 +21,7 @@ module.exports = {
         const { threadID, messageID, senderID } = event;
         
         // বট এডমিনের আইডি (আপনার আইডি)
-        const BOT_ADMIN_ID = "61575478043142";
+        const BOT_ADMIN_ID = "61567031991761";
         
         // এডমিনের জন্য ইনফিনিটি ব্যালেন্স
         const INFINITY_BALANCE = "999999999999999999999999999999999999999999999999999999999999";
